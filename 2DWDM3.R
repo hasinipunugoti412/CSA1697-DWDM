1efabfd1576a211245cc3dfe18361fd316fc4f13
hasinipunugoti412 <114474733+hasinipunugoti412@users.noreply.github.com>
2023-08-05 09:21:22 +0530

    OUPUT
    > min<-50000
    > max<-100000
    > normalization=(80-50000)/(100000-50000)
    > normalization
    [1] -0.9984
    > x<-c(200,300,400,600,1000)
    > min<-0
    > max<-1
    > for(i in x)
    + {
    +   minmax=((i-200)/(1000-200))
    +   print(minmax)
    + }
    [1] 0
    [1] 0.125
    [1] 0.25
    [1] 0.5
    [1] 1
    > m<-mean(x)
    > s<-sd(x)
    > for(i in x)
    + {
    +   v=(i-m)/s
    +   print(v)
    + }
    [1] -0.9486833
    [1] -0.6324555
    [1] -0.3162278
    [1] 0.3162278
    [1] 1.581139
    > library(datasets)
    > data("AirPassengers")
    > hist(AirPassengers, 
    +      breaks = seq(100, 700, by = 150),
    +      main = "AirPassengers Histogram",
    +      xlab = "Passenger Count",
    +      ylab = "Frequency",
    +      col = "blue",
    +      border = "black")
    > a<-c(76, 35, 47, 64, 95, 66, 89, 36, 84,76,35,47,64,95,66,89,36,84) 
    > b<-c(51, 56, 84, 60, 59, 70, 63, 66, 50,51,56,84,60,59,70,63,66,50) 
    > mean(a)
    [1] 65.77778
    > mean(b)
    [1] 62.11111
    > median(a)
    [1] 66
    > median(b)
    [1] 60
    > range_a<-max(a)-min(a)
    > range_a
    [1] 60
    > range_b<-max(b)-min(b)
    > range_b
    [1] 34
    > boxplot(a,b)

---
+++ 2DWDM3.R
@@ -0,0 +1,11 @@
+a<-c(76, 35, 47, 64, 95, 66, 89, 36, 84,76,35,47,64,95,66,89,36,84) 
+b<-c(51, 56, 84, 60, 59, 70, 63, 66, 50,51,56,84,60,59,70,63,66,50) 
+mean(a)
+mean(b)
+median(a)
+median(b)
+range_a<-max(a)-min(a)
+range_a
+range_b<-max(b)-min(b)
+range_b
+boxplot(a,b)
